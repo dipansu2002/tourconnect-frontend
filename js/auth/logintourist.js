8977cf07b120e5e6cbdc53ccbd5ff7649ea420b6
dipansu2002 <luckysingh8823@gmail.com>
2024-09-01 15:34:28 +0530

    Initial commit

---
+++ js/auth/logintourist.js
@@ -0,0 +1,33 @@
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    try {
+        // send request
+        const response = await fetch('http://localhost:5555/auth/logintourist', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        // response
+        const result = await response.json();
+        if (response.ok) {
+            // Store the token and userId in localStorage
+            localStorage.setItem('x-auth-token', result.token);
+            localStorage.setItem('userId', result.userId);
+            
+            alert('Login successful!');
+            // Redirect to dashboard
+            window.location.href = 'dashboardtourist.html'; // Ensure the path is correct
+        } else {
+            alert(`Error: ${result.message}`);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
---
+++ js/router.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const routes = {
+        '/': 'index.html',
+        '/register': 'pages/registertourist.html',
+        '/login': 'pages/logintourist.html',
+        '/dashboard': 'pages/dashboardtourist.html',
+    };
+
+    function navigateTo(path) {
+        if (routes[path]) {
+            fetch(routes[path])
+                .then(response => response.text())
+                .then(html => {
+                    document.querySelector('main').innerHTML = html;
+                    window.history.pushState({ path }, '', path);
+                });
+        }
+    }
+
+    // Event listener for browser navigation (back/forward buttons)
+    window.addEventListener('popstate', (event) => {
+        // Check if there is state information
+        if (event.state) {
+            // Navigate to the path stored in the state
+            navigateTo(event.state.path);
+        }
+    });
+
+    // Add click event listeners to all anchor tags
+    document.querySelectorAll('a').forEach(anchor => {
+        anchor.addEventListener('click', (event) => {
+            // Prevent the default anchor behavior (page reload)
+            event.preventDefault();
+            // Navigate to the path of the clicked anchor
+            navigateTo(new URL(event.target.href).pathname);
+        });
+    });
+
+    // Initial navigation based on the current URL path
+    navigateTo(window.location.pathname);
+});
